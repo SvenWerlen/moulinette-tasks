7c16d74cf1c43839a2b9514946df3cc57fc1c1f5
Sven Werlen <sven.werlen@pm.me>
2021-05-02 06:57:23 -0400

    Build with conditions

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [ -n "$AZURE_STORAGE_CONTAINER" ]; then 
+
+  if [ -n "$TASK_ID" ]; then 
+    
+    sudo mkdir /mnt/blobfusetmp
+    sudo chown appveyor /mnt/blobfusetmp
+    sudo mount -t tmpfs -o size=16g tmpfs /mnt/blobfusetmp
+    sudo mkdir /mnt/blob
+    sudo chown appveyor /mnt/blob
+    blobfuse /mnt/blob --tmp-path=/mnt/blobfusetmp -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --file-cache-timeout-in-seconds=120 --container-name=$AZURE_STORAGE_CONTAINER
+    AZURE_STORAGE_MOUNT=/mnt/blob python3 ./processTasks.py
+  
+  else
+    echo "TASK_ID is not defined or empty. Skipping..."
+  fi
+
+else
+  echo "AZURE_STORAGE_CONTAINER is not defined or empty. Skipping..."
+fi
+
+
--- processTasks.py
+++ processTasks.py
@@ -9,6 +9,7 @@ from time import time
 MOULINETTE_API = os.getenv('MOULINETTE_API')
 MOULINETTE_SECRET_KEY = os.getenv('MOULINETTE_SECRET')
 AZURE_MOUNT = os.getenv('AZURE_STORAGE_MOUNT')
+TASK_ID = os.getenv('TASK_ID')
 TMP = "/tmp/"
 
 ## Utility function to delete a task when completed
@@ -32,61 +33,59 @@ data = response.json()
 token = data["access_token"]
 
 # Get tasks
-response = requests.get(url = MOULINETTE_API + "/tasks", headers = {"Content-Type": "application/json", "Authorization": "Bearer " + token})
+response = requests.get(url = MOULINETTE_API + "/task/" + TASK_ID, headers = {"Content-Type": "application/json", "Authorization": "Bearer " + token})
 
 if response.status_code != 200:
   sys.exit("Get tasks failed! " + response.text)
 
-tasks = response.json()
+task = response.json()
+id = task["id"]
 
-for t in tasks:
-  id = t["id"]
-  
-  # TASK Extract
-  if t["type"] == "extract":
-    blob = t["data"]["blob"]
-    filepath = os.path.join(AZURE_MOUNT, blob)
-    if os.path.isfile(filepath):
-      dir = os.path.splitext(os.path.basename(filepath))[0]
-      dirpath = os.path.join(AZURE_MOUNT, dir)
-      tmppath = os.path.join(TMP, dir)
-      if os.path.isdir(dirpath):
-        print("Dir %s already exists" % dirpath)
-      else:
-        # prepare
-        if os.path.isdir(tmppath):
-          os.system("rm -rf '%s'" % tmppath)
-        os.mkdir(tmppath)
-        
-        # extract archive
-        secs = time()
-        os.system("unzip -q %s -d %s" % (filepath, tmppath))
-        print("Unzipped in %.1f seconds" % (time() - secs))
-        
-        # change permissions (just in case)
-        os.system("chmod -R 755 %s" % tmppath)
-        
-        # convert images to webp
-        secs = time()
-        os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \) -exec cwebp -quiet '{}' -o '{}.webp' \;" % tmppath)
-        print("Conversion to webp in %.1f seconds" % (time() - secs))
-        
-        # delete original files, rename webp files and remove all non-images
-        secs = time()
-        os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.gif -o -iname \*.jpeg \) -exec rm '{}' \;" % tmppath)
-        os.system("find '%s' -type f -not -iname *.webp -exec rm '{}' \;" % tmppath)
-        os.system("find '%s' -type f -iname *.webp -exec python3 rename.py '{}' \;" % tmppath)
-        print("Cleanup in %.1f seconds" % (time() - secs))
-        
-        # move files to cloud
-        secs = time()
-        os.system("mv '%s' '%s'" % (tmppath, dirpath))
-        print("Copied to storage in %.1f seconds" % (time() - secs))
-        
-      # Complete task
-      completeTask(t["id"])
-        
+# TASK Extract
+if task["type"] == "extract":
+  blob = task["data"]["blob"]
+  filepath = os.path.join(AZURE_MOUNT, blob)
+  if os.path.isfile(filepath):
+    dir = os.path.splitext(os.path.basename(filepath))[0]
+    dirpath = os.path.join(AZURE_MOUNT, dir)
+    tmppath = os.path.join(TMP, dir)
+    if os.path.isdir(dirpath):
+      print("Dir %s already exists" % dirpath)
     else:
-      print("Blob %s doesn't exist !" % blob)
+      # prepare
+      if os.path.isdir(tmppath):
+        os.system("rm -rf '%s'" % tmppath)
+      os.mkdir(tmppath)
+      
+      # extract archive
+      secs = time()
+      os.system("unzip -q %s -d %s" % (filepath, tmppath))
+      print("Unzipped in %.1f seconds" % (time() - secs))
+      
+      # change permissions (just in case)
+      os.system("chmod -R 755 %s" % tmppath)
+      
+      # convert images to webp
+      secs = time()
+      os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \) -exec cwebp -quiet '{}' -o '{}.webp' \;" % tmppath)
+      print("Conversion to webp in %.1f seconds" % (time() - secs))
+      
+      # delete original files, rename webp files and remove all non-images
+      secs = time()
+      os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.gif -o -iname \*.jpeg \) -exec rm '{}' \;" % tmppath)
+      os.system("find '%s' -type f -not -iname *.webp -exec rm '{}' \;" % tmppath)
+      os.system("find '%s' -type f -iname *.webp -exec python3 rename.py '{}' \;" % tmppath)
+      print("Cleanup in %.1f seconds" % (time() - secs))
+      
+      # move files to cloud
+      secs = time()
+      os.system("mv '%s' '%s'" % (tmppath, dirpath))
+      print("Copied to storage in %.1f seconds" % (time() - secs))
+      
+    # Complete task
+    completeTask(task["id"])
+      
+  else:
+    print("Blob %s doesn't exist !" % blob)
     
 
