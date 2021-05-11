9fd53ddd8fd4527a5172b2693da189fb08cb8733
Sven Werlen <sven.werlen@pm.me>
2021-05-10 21:17:57 -0400

    Fix & improve security

--- build.sh
+++ build.sh
@@ -11,8 +11,9 @@ if [ -n "$AZURE_STORAGE_CONTAINER" ]; then
     sudo mkdir /mnt/blob
     sudo chown appveyor /mnt/blob
     blobfuse /mnt/blob --tmp-path=/mnt/blobfusetmp -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --file-cache-timeout-in-seconds=120 --container-name=$AZURE_STORAGE_CONTAINER
-    AZURE_STORAGE_MOUNT=/mnt/blob python3 ./processTask.py
+    AZURE_STORAGE_MOUNT=/mnt/blob python3 AZURE_STORAGE_ACCESS_KEY="" AZURE_STORAGE_ACCOUNT="" ./processTask.py
     python3 ./completeTask.py
+    sudo umount /mnt/blob
   
   else
     echo "TASK_ID is not defined or empty. Skipping..."
--- processTask.py
+++ processTask.py
@@ -56,77 +56,81 @@ if task["type"] == "extract":
     dir = os.path.splitext(os.path.basename(filepath))[0]
     dirpath = os.path.join(AZURE_MOUNT, dir)
     tmppath = os.path.join(TMP, dir)
+    
+    # clear existing blobs (if any)
     if os.path.isdir(dirpath):
-      print("Dir %s already exists" % dirpath)
-    else:
-      # prepare
-      if os.path.isdir(tmppath):
-        os.system("rm -rf '%s'" % tmppath)
-      os.mkdir(tmppath)
-      
-      # extract archive
-      secs = time()
-      os.system("unzip -q %s -d %s" % (filepath, tmppath))
-      print("Unzipped in %.1f seconds" % (time() - secs))
-      
-      # change permissions (just in case)
-      os.system("chmod -R 755 %s" % tmppath)
-      
-      # special case for webm (videos) => check if matching webp file exists in same folder
-      thumbsToDelete = []
-      for root, dirs, files in os.walk(tmppath):
-        for file in files:
-          if file.endswith(".webm"):
-            thumb = os.path.join(root, os.path.splitext(file)[0] + ".webp")
-            # thumbnail already exists
-            if os.path.isfile(thumb):
+      os.system("rm -rf '%s'" % dirpath)
+    
+    # prepare
+    if os.path.isdir(tmppath):
+      os.system("rm -rf '%s'" % tmppath)
+    os.mkdir(tmppath)
+    
+    # extract archive
+    secs = time()
+    os.system("unzip -q %s -d %s" % (filepath, tmppath))
+    print("Unzipped in %.1f seconds" % (time() - secs))
+    
+    # change permissions (just in case)
+    os.system("chmod -R 755 %s" % tmppath)
+    
+    # special case for webm (videos) => check if matching webp file exists in same folder
+    thumbsToDelete = []
+    for root, dirs, files in os.walk(tmppath):
+      for file in files:
+        if file.endswith(".webm"):
+          thumb = os.path.join(root, os.path.splitext(file)[0] + ".webp")
+          # thumbnail already exists
+          if os.path.isfile(thumb):
+            continue
+          
+          # look for a matching webp file
+          found = None
+          for f in os.listdir(root):
+            if not f.endswith(".webp"):
               continue
-            
-            # look for a matching webp file
-            found = None
-            for f in os.listdir(root):
-              if not f.endswith(".webp"):
-                continue
-              # filename is included (ex: myvideo-200x200.webm and myvideo.webp)
-              if file.startswith(os.path.splitext(f)[0]):
-                found = os.path.join(root, f)
-                break
-              # filename is included when removing the last _... part (ex: myvideo-200x200.webm and myvideo_thumb.webp)
-              underscore = f.rfind('_')
-              if underscore > 0 and file.startswith(f[0:underscore]):
-                found = os.path.join(root, f)
-                break
-            
-            if found:
-              shutil.copyfile(found, thumb)
-              if not found in thumbsToDelete:
-                thumbsToDelete.append(found)
-              print("Matching thumbnail found for video: %s" %  os.path.join(root, file))
-            else:
-              print("No matching thumbnail found for video: %s" %  os.path.join(root, file))
-      
-      # remove all thumbnails (to avoid them to appear in Foundry)
-      for t in thumbsToDelete:
-        os.remove(t)
-      
-      # convert images to webp
-      secs = time()
-      os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \) -exec cwebp -quiet '{}' -o '{}.webp' \;" % tmppath)
-      print("Conversion to webp in %.1f seconds" % (time() - secs))
-      
-      # delete original files, rename webp files and remove all non-supported files
-      secs = time()
-      os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.gif -o -iname \*.jpeg \) -exec rm '{}' \;" % tmppath)
-      os.system("find '%s' -type f -not -iname *.webp -not -iname *.webm -exec rm '{}' \;" % tmppath)
-      os.system("find '%s' -type f -iname *.webp -exec python3 rename.py '{}' \;" % tmppath)
-      print("Cleanup in %.1f seconds" % (time() - secs))
-      
-      # move files to cloud
-      secs = time()
-      os.system("mv '%s' '%s'" % (tmppath, dirpath))
-      print("Copied to storage in %.1f seconds" % (time() - secs))
+            # filename is included (ex: myvideo-200x200.webm and myvideo.webp)
+            if file.startswith(os.path.splitext(f)[0]):
+              found = os.path.join(root, f)
+              break
+            # filename is included when removing the last _... part (ex: myvideo-200x200.webm and myvideo_thumb.webp)
+            underscore = f.rfind('_')
+            if underscore > 0 and file.startswith(f[0:underscore]):
+              found = os.path.join(root, f)
+              break
+          
+          if found:
+            shutil.copyfile(found, thumb)
+            if not found in thumbsToDelete:
+              thumbsToDelete.append(found)
+          else:
+            # try to generate a new thumbnail from the video
+            os.system("avconv -v -8 -ss 2 -i %s -frames:v 1 %s" % (os.path.join(root, file), thumb))
+    
+    # remove all thumbnails (to avoid them to appear in Foundry)
+    for t in thumbsToDelete:
+      os.remove(t)
+    
+    # convert images to webp
+    secs = time()
+    os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \) -exec cwebp -quiet '{}' -o '{}.webp' \;" % tmppath)
+    print("Conversion to webp in %.1f seconds" % (time() - secs))
+    
+    # delete original files, rename webp files and remove all non-supported files
+    secs = time()
+    os.system("find '%s' -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.gif -o -iname \*.jpeg \) -exec rm '{}' \;" % tmppath)
+    os.system("find '%s' -type f -not -iname *.webp -not -iname *.webm -exec rm '{}' \;" % tmppath)
+    os.system("find '%s' -type f -iname *.webp -exec python3 rename.py '{}' \;" % tmppath)
+    print("Cleanup in %.1f seconds" % (time() - secs))
+    
+    # move files to cloud
+    secs = time()
+    os.system("mv '%s' '%s'" % (tmppath, dirpath))
+    print("Copied to storage in %.1f seconds" % (time() - secs))
       
   else:
     print("Blob %s doesn't exist !" % blob)
     
-
+# cleanup
+if os.path.isdir(tmppath):
+  os.system("rm -rf '%s'" % tmppath)
