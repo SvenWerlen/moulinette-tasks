7e27c0a5ea76ef9f4696bcdc4e60381b01a8cbc8
Sven Werlen <sven.werlen@pm.me>
2021-05-10 21:33:16 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ if [ -n "$AZURE_STORAGE_CONTAINER" ]; then
     sudo mkdir /mnt/blob
     sudo chown appveyor /mnt/blob
     blobfuse /mnt/blob --tmp-path=/mnt/blobfusetmp -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --file-cache-timeout-in-seconds=120 --container-name=$AZURE_STORAGE_CONTAINER
-    AZURE_STORAGE_MOUNT=/mnt/blob python3 AZURE_STORAGE_ACCESS_KEY="" AZURE_STORAGE_ACCOUNT="" ./processTask.py
+    AZURE_STORAGE_MOUNT=/mnt/blob AZURE_STORAGE_ACCESS_KEY="" AZURE_STORAGE_ACCOUNT="" python3 ./processTask.py
     python3 ./completeTask.py
     sudo umount /mnt/blob
   
