bf6ee07b697d86b86c35e1f562d172a0b7d40b30
Sven Werlen <sven.werlen@pm.me>
2021-09-28 23:03:28 -0400

    Fix subfolders

--- sunzip.sh
+++ sunzip.sh
@@ -25,12 +25,9 @@ find $2 -name "__MACOSX*" -exec rm -rf {} \;
 FILES=$(find $2 -maxdepth 1 -type f | wc -l)
 DIRS=$(find $2 -maxdepth 1 -type d | wc -l)
 
-if [ ! "$FILES" -eq "0" ] || [ ! "$DIRS" -eq "1" ]; then
+if [ ! "$FILES" -eq "0" ] || [ ! "$DIRS" -eq "2" ]; then
   subfolder="$2/$(basename "$1" .zip)"
   echo "Moving all files to $subfolder..."
   mkdir "$subfolder"
   mv "$2"/* "$subfolder/"
 fi
-
-
-
