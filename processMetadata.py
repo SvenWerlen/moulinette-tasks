2edaf1a0466e73899713d8c51f51585f93033e65
Sven Werlen <sven.werlen@pm.me>
2023-09-24 15:13:50 -0400

    Fix path when not in DEBUG mode

--- processMetadata.py
+++ processMetadata.py
@@ -8,8 +8,14 @@ import json
 import logging
 
 # Get required variable environment
-TASKS_FILE   = "moulinette-tasks.json"
+TASKS_FILE    = "moulinette-tasks.json"
+OUTPUT_FOLDER = os.getenv('OUTPUT_FOLDER') # Output folder (where to download the pack)
 TMP = "/tmp/"
+DEBUG = False
+
+# Check if debug mode enabled
+if len(sys.argv) > 1:
+  DEBUG = sys.argv[1] == "true"
 
 # logging information
 logging.basicConfig(
@@ -31,6 +37,10 @@ if len(tasks) > 0:
   task = tasks[0]
   blob = task["packFile"]
   container = task["container"]
+  packName = os.path.splitext(blob)[0]
+  targetFolder = os.path.join(OUTPUT_FOLDER, container, packName)
+  if DEBUG:
+    targetFolder = os.path.join(TMP, "mtte", packName)
 
   ## Support for Michael Ghelfi
   ## Up to 2 folders => category
@@ -39,14 +49,13 @@ if len(tasks) > 0:
   
     allSounds = { 'Music': [] }
 
-    dir = os.path.splitext(os.path.basename(blob))[0]
-    metadataPath = os.path.join(TMP, "mtte", dir, "metadata.json")
+    metadataPath = os.path.join(targetFolder, "metadata.json")
     if os.path.exists(metadataPath):
       logger.warn(f"[ProcessMeta] Metadata file already exists in pack {blob}")
     else:
       try:
         audios = {}
-        with open(os.path.join(TMP, "mtte", dir, "audioInfo.json")) as f:
+        with open(os.path.join(targetFolder, "audioInfo.json")) as f:
           audios = json.load(f)
 
         for path in audios.keys():
