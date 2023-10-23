dce75d9c72da7b7a6c0f6046a755700adb2f7b77
Sven Werlen <sven.werlen@pm.me>
2023-10-22 21:32:02 -0400

    Wip MongoDB

---
+++ updateMongoDB.py
@@ -0,0 +1,71 @@
+###
+### This scripts gets the task to update MongoDB with all the assets
+###
+import os
+import sys
+import json
+import shutil
+from pymongo import MongoClient
+import requests
+import logging
+import boto3
+
+# Get required environment variables
+OUTPUT_FOLDER = os.getenv('OUTPUT_FOLDER') # Output folder (where data is ready for upload)
+MONGODB_URI   = f"mongodb+srv://{os.environ['MONGO_CREDS']}/?retryWrites=true&w=majority"
+
+# logging information
+logging.basicConfig(
+    format="[%(levelname)s] %(message)s (%(filename)s, %(funcName)s(), line %(lineno)d)",
+    level=logging.WARN,
+)
+logger = logging.getLogger("moulinette_utils")
+logger.setLevel(logging.INFO)
+
+
+# Constants
+TASKS_STATUS = "moulinette-tasks-status.json"
+TMP = "/tmp/"
+
+# Check output folder
+if not OUTPUT_FOLDER or not os.path.isdir(OUTPUT_FOLDER):
+  sys.exit(f'[UpdateMongoDB] {OUTPUT_FOLDER} is not a valid directory')
+
+# Check tasks (input)
+if not os.path.isfile(os.path.join(TMP, TASKS_STATUS)):
+  sys.exit(f'[UpdateMongoDB] no {TASKS_STATUS} file found')
+
+tasks = []
+with open(os.path.join(TMP, TASKS_STATUS)) as f:
+  tasks = json.load(f)
+
+# Only process 1 task at a time
+if len(tasks) > 0:
+  task = tasks[0]
+  
+  if task["status"] and task["status"] == "done":
+    logger.info(f'Updating DB for {task["packFile"]} from task {task["id"]}')
+    
+    packName = os.path.splitext(task["packFile"])[0]
+    assetsFile = os.path.join(OUTPUT_FOLDER, task["container"], packName, "json", "assets.json")
+
+    if not os.path.isfile(assetsFile):
+      sys.exit(f'No {assetsFile} file found')
+
+    with open(assetsFile, "r") as f:
+      assets = json.load(f)
+
+      client = MongoClient(MONGODB_URI)
+      client.admin.command('ping')
+      db = client.moulinette
+      coll = db.assets
+      
+      # removing existing entries
+      unique = { 'creatorId': task["container"], 'packId': task["packFile"] }
+      result = coll.delete_many(unique)
+      logger.info(f"{result.deleted_count} documents ont été supprimés avec succès (MongoDB).")
+
+      # upload to MongoDB
+      result = coll.insert_many(assets)
+      if result.inserted_ids:
+        logger.info(f"{len(result.inserted_ids)} documents insérés avec succès (MongoDB).")
